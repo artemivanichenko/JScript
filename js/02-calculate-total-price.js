da743b5e91662c334956972b2fe1e5347afe35c7
Artem <ivanichenko.ua@gmail.com>
2023-01-27 16:03:48 +0200

    aaa

--- js/02-calculate-total-price.js
+++ js/02-calculate-total-price.js
@@ -12,20 +12,36 @@
 
 // console.log('Total: ', total);
 
-const calculateTotalPrice = function (items) {
-  console.log('items внутри функции: ', items);
+// const calculateTotalPrice = function (items) {
+//   console.log('items внутри функции: ', items);
 
-  let total = 0;
+//   let total = 0;
 
-  for (const item of items) {
-    total += item;
-  }
+//   for (const item of items) {
+//     total += item;
+//   }
 
-  return total;
-};
+//   return total;
+// };
 
-const r1 = calculateTotalPrice([1, 2, 3]);
+// const r1 = calculateTotalPrice([1, 2, 3]);
 
-console.log(`Общая сумма покупок ${r1}`); // 6
-console.log(calculateTotalPrice([5, 10, 15, 20])); // 50
-console.log(calculateTotalPrice([100, 200, 300])); // 600
+// console.log(`Общая сумма покупок ${r1}`); // 6
+// console.log(calculateTotalPrice([5, 10, 15, 20])); // 50
+// console.log(calculateTotalPrice([100, 200, 300])); // 600
+
+function getExtremeElements(array) {
+  // Change code below this line
+  //   const resultArray = array[0];
+  //   console.log('HI');
+  console.log(array);
+
+  //   return resultArray;
+  // Change code above this line
+  return rrr[(array[0], array[array.length - 1])];
+}
+
+// getExtremeElements([1, 2, 3, 4]);
+// console.log(resultArray);
+// getExtremeElements([1, 2, 3, 4]);
+console.log(rrr);
